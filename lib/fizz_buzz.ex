be34d2a639e2d3196a40a170eee27549081082ab
costadv <dvcosta0505@outlook.com>
2026-08-28 18:30:22 -0300

    Created function handleFileRead with pattern matching as parameter and removed case from build function. Added pipeline execution inside build function.

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,10 +1,17 @@
 defmodule FizzBuzz do
   def build(fileName) do
-    case File.read(fileName) do
-      {:ok, file} -> file
-      {:error, _} -> "File not found."
-    end
+    #case File.read(fileName) do
+    #  {:ok, file} -> file
+    #  {:error, _} -> "File not found."
+    #end
+
+    File.read(fileName)
+    |> handleFileRead()
+
   end
+
+  def handleFileRead({:error, reason}), do: "File not found. Reason: #{reason}"
+  def handleFileRead({:ok, fileContent}), do: fileContent
 
 end
 
