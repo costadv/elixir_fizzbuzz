129a5a01812e65aa78fa8258841f72502ffc316b
costadv <dvcosta0505@outlook.com>
2026-09-01 12:54:49 -0300

    Replaced fizzBuzz function with evaluateNumber function to use guards and pattern matching.

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -13,18 +13,23 @@ defmodule FizzBuzz do
     |> Enum.map(&String.trim/1)
     |> Enum.map(&String.to_integer/1)
     |> IO.inspect()
-    |> Enum.map(&fizzBuzz/1)
+    |> Enum.map(&evaluateNumber/1)
   end
 
-  def fizzBuzz(number) do
-    cond do
-      Integer.mod(number, 3) + Integer.mod(number, 5) == 0 -> :FizzBuzz
-      Integer.mod(number, 3) == 0 -> :Fizz
-      Integer.mod(number, 5) == 0 -> :Buzz
-      true -> number
-    end
+  def evaluateNumber(number) when rem(number, 3) == 0, do: :fizz
+  def evaluateNumber(number) when rem(number, 5) == 0, do: :buzz
+  def evaluateNumber(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def evaluateNumber(number), do: number
 
-  end
+  #def fizzBuzz(number) do
+  #  cond do
+  #    Integer.mod(number, 3) + Integer.mod(number, 5) == 0 -> :fizzBuzz
+  #    Integer.mod(number, 3) == 0 -> :fizz
+  #    Integer.mod(number, 5) == 0 -> :buzz
+  #    true -> number
+  #  end
+  #
+  #end
 
 end
 
