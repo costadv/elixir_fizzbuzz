d2ece7b36e4a0285e5699a909250f537436eaaac
costadv <dvcosta0505@outlook.com>
2026-09-01 12:58:17 -0300

    Fixed issue where fizzbuzz case in evaluateNumber wasn't being triggered.

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -16,9 +16,9 @@ defmodule FizzBuzz do
     |> Enum.map(&evaluateNumber/1)
   end
 
+  def evaluateNumber(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
   def evaluateNumber(number) when rem(number, 3) == 0, do: :fizz
   def evaluateNumber(number) when rem(number, 5) == 0, do: :buzz
-  def evaluateNumber(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
   def evaluateNumber(number), do: number
 
   #def fizzBuzz(number) do
